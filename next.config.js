a12d82022164534097f814e11360ce40a81734c1
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-16 07:48:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const { withContentlayer } = require('next-contentlayer');
 
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/posts',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
